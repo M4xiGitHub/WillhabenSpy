9c36f6fefee7d85bd346dc64587b46f0d2b1747b
M4xiGitHub <maximilian.bleier75@gmail.com>
2020-06-24 15:25:12 +0200

    Beautify

--- main.js
+++ main.js
@@ -1,125 +1,116 @@
-
 var config;
 var json = {};
 
 $.getJSON("data.json", function(json) {
 
-var keys = Object.keys(json)
-keys.shift()
+    var keys = Object.keys(json)
+    keys.shift()
+    var dataset = []
 
-var dataset = []
+    json["Products"].forEach(value => {
+        dataset.push({
+            label: value,
+            data: [],
+            borderColor: ['#' + Math.random().toString(16).substr(2, 6)],
+            borderWidth: 1
+        })
+    });
 
-json["Products"].forEach(value =>{
-    dataset.push({
-        label:value, 
-        data:[],
-        borderColor: [
-                '#'+Math.random().toString(16).substr(2,6),
-                ],
-        borderWidth: 1})
-});
-
-keys.forEach(value => {
-    var i = 0;
-    while(i < json[value].length){
-        dataset[i]["data"].push(json[value][i]);
-        console.log(dataset[i]["data"])
-        i++;
-    } 
-});
-
-
-    
-config = {
-    type: 'line',
-    data: {
-        labels: keys ,
-        datasets: dataset
-    },
-    options: 
-    {
-        legend: {
-            labels: {
-                fontColor: "white",
-                fontSize: 18
-            }
-        },
-        responsive: true,
-        title: {
-            display: true,
-            text: "WillhabenSpy",
-            fontColor: "white",
-            fontSize: 25
-        },
-        tooltips: {
-            mode: 'label',
-        },
-        hover: {
-            mode: 'nearest',
-            intersect: true
-        },
-        
-        scales: {
-            xAxes: [{
-                display: true,
-                gridLines: {
-                    display: false
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Zeitpunkt',
-                    fontColor: "white",
-                    fontSize: 18
-                }, ticks: {
-                    fontColor: "white",
-                    fontSize: 14,
-                    stepSize: 1,
-                    beginAtZero: true
-                }
-            }],
-            yAxes: [{
-                display: true,
-
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Preis',
-                    fontColor: "white",
-                    fontSize: 18
-                },
-                ticks: {
-                    fontColor: "white",
-                    fontSize: 14,
-                    stepSize: 100,
-                    beginAtZero: true
-                }, gridLines: {
-                    color: "#002d43"
-                }
-            }]
-        
-            
-        },
-        plugins: {
-            zoom: {
-                
-                zoom: {
-                    enabled: true,
-
-                    mode: 'xy',
-                }
-            }
+    keys.forEach(value => {
+        var i = 0;
+        while (i < json[value].length) {
+            dataset[i]["data"].push(json[value][i]);
+            console.log(dataset[i]["data"])
+            i++;
         }
-    
-    }
-    
-};
-var ctx = document.getElementById("myChart").getContext("2d");
-window.myLine = new Chart(ctx, config);
+    });
 
 
 
+    config = {
+        type: 'line',
+        data: {
+            labels: keys,
+            datasets: dataset
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontColor: "white",
+                    fontSize: 18
+                }
+            },
+            responsive: true,
+            title: {
+                display: true,
+                text: "WillhabenSpy",
+                fontColor: "white",
+                fontSize: 25
+            },
+            tooltips: {
+                mode: 'label',
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Zeitpunkt',
+                        fontColor: "white",
+                        fontSize: 18
+                    },
+                    ticks: {
+                        fontColor: "white",
+                        fontSize: 14,
+                        stepSize: 1,
+                        beginAtZero: true
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Preis',
+                        fontColor: "white",
+                        fontSize: 18
+                    },
+                    ticks: {
+                        fontColor: "white",
+                        fontSize: 14,
+                        stepSize: 100,
+                        beginAtZero: true
+                    },
+                    gridLines: {
+                        color: "#002d43"
+                    }
+                }]
 
 
+            },
+            plugins: {
+                zoom: {
+
+                    zoom: {
+                        enabled: true,
+
+                        mode: 'xy',
+                    }
+                }
+            }
+
+        }
+
+    };
+    var ctx = document.getElementById("myChart").getContext("2d");
+    window.myLine = new Chart(ctx, config);
 
 });
-
-
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+Bleier Maximilian
+24.06.2020
+"""
+
 import urllib.request
 from lxml import html as lh
 import json
@@ -6,29 +11,31 @@ import codecs
 import time
 
 
-
+#Reads the File
 def readFile(file):
     f = open(file,"r")
     return f.readlines() 
 
 
-
+#Gets Price from Product from Willhaben
 def getPrice(html):
     element = '//*[@id="skip-to-content"]/article/section[3]/div/div[3]/div/div[1]/div[1]/div/span[1]'   #Price        
     xml = lh.fromstring(html)
     return xml.xpath(element)[0].text_content().replace("€","").replace(".","")
 
+#Gets Name from Product from Willhaben
 def getName(html):
     element = '//*[@id="skip-to-content"]/article/section[3]/div/div[1]/div/div[1]/h1'   #Name        
     xml = lh.fromstring(html)
     return xml.xpath(element)[0].text_content()
 
+#Saves the array to data.json to load in js
 def export(out):
     with codecs.open('data.json', 'w+', encoding="utf-8") as outfile:
         json.dump(out, outfile, ensure_ascii=False)
     return  
 
-
+#Loads the array in to update it
 def load():
     with open('data.json',encoding='utf-8') as json_file:
         return(json.load(json_file))
